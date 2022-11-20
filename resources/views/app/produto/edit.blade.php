a3add0479241fa30d3453c23f11797f2511ef93c
kevenfraga <kevenroger@gmail.com>
2022-11-20 20:34:46 -0300

    Crud para produto

--- resources/views/app/produto/edit.blade.php
+++ resources/views/app/produto/edit.blade.php
@@ -4,7 +4,7 @@
 @section('conteudo')
     <div class="conteudo-pagina">
         <div class="titulo-pagina-2">
-            <p>Adicionar Produto</p>
+            <p>Editar Produto</p>
         </div>
 
         <div class="menu">
@@ -15,8 +15,8 @@
         </div>
 
         <div class="informacao-pagina">
-            <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                @component('app.produto._components.form_create_edit', ['unidades' => $unidades])
+            <div style="width: 30%; margin-left: auto; margin-right: auto">
+                @component('app.produto._components.form_create_edit', ['produto' => $produto, 'unidades' => $unidades])
                 @endcomponent
             </div>
         </div>
--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -25,6 +25,7 @@
                             <th>Unidade ID</th>
                             <th></th>
                             <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -34,8 +35,16 @@
                                 <td>{{ $produto->descricao }}</td>
                                 <td>{{ $produto->peso }}</td>
                                 <td>{{ $produto->unidade_id }}</td>
-                                <td><a href="">Excluir</a></td>
-                                <td><a href="">Editar</a></td>
+                                <td><a href="{{ route('produto.show', ['produto' => $produto->id]) }}">Visualizar</a></td>
+                                <td>
+                                    <form id="form_{{ $produto->id }}" method="POST" action="{{ route('produto.destroy', ['produto' => $produto->id]) }}">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <a href="#" onclick="document.getElementById('form_{{ $produto->id }}').submit()">Excluir</a>
+                                    </form>
+                                </td>
+                                <td><a href="{{ route('produto.edit', ['produto' => $produto->id]) }}">Editar</a></td>
                             </tr>
                         @endforeach
                     </tbody>
